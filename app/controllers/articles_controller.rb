63d152a616b571ffd9caab89e23de9313eedb6c6
Lozitskiy Vladimir <loza-w@mail.ru>
2016-06-17 14:12:06 +0300

    Add writing of article to bd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,19 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles  = Article.all
+  end
+
   def new
   end
 
   def create
       render plain: params[:article].inspect
+      @article = Article.new(article_params)
+      @article.save
   end
+end
+
+def article_params
+  params.require(:article).permit(:id, :title, :text)
 end
