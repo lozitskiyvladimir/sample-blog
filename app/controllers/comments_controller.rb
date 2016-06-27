7d7a34183bf899cf2f6ccced37df882e18d6b5c8
Lozitskiy Vladimir <loza-w@mail.ru>
2016-06-27 15:22:44 +0300

    Add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,16 @@
 class CommentsController < ApplicationController
   def create
-      
+    @article = Article.find(params[:article_id])
+    @article.comments.create (comment_params)
+
+    redirect_to article_path(@article)
   end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:id, :author, :body)
+  end
+
 end
