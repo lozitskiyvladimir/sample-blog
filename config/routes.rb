294790c5d567b0a5cecfb8b1659d81929237eee1
Lozitskiy Vladimir <loza-w@mail.ru>
2016-06-27 11:00:18 +0300

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
   resource  :contacts, only:[:new, :create], path_names: {:new => ''}
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
 
 
