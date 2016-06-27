f112817afdd2fec0cbf61006982b7e9ab962d467
Lozitskiy Vladimir <loza-w@mail.ru>
2016-06-27 16:08:24 +0300

    Reject unuseble routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   resource  :contacts, only:[:new, :create], path_names: {:new => ''}
   resources :articles do
-    resources :comments
+    resource  :comments, only:[ :create]
   end
 
 
