721cf2eac6390c0ad12b4231ac36963782406540
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-01 15:17:10 +0300

    Update Article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :article do
     title 'article title'
     text  'article text'
+
+    factory :article_with_comments do
+      after :create do |article, evaluater|
+        create_list :comment, 3, article: article
+      end  
+    end
   end
 end
