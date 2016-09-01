685ca38ceac6f6adc8b7ebce5311780bccdbaaca
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-01 17:05:53 +0300

    Update article_spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,10 @@ describe Article do
   describe "validation" do
     it {should validate_presence_of (:title)}
     it {should validate_presence_of (:text)}
+# I've used ensure_length_of becouse it works in latest versions
+    it { should ensure_length_of(:title).is_at_most(140) }
+    it { should ensure_length_of(:text).is_at_most(4000) }
+
   end
 
   describe "associations" do
