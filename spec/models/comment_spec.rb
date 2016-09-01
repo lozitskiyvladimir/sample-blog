bb824e0b45dd119fe67f27c049eed63cd49e5bc0
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-01 17:06:17 +0300

    Update comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 
 describe Comment do
   it { should belong_to(:article) }
+
+    describe "validations" do
+      it {should ensure_length_of(:body).is_at_most(4000)}
+    end
 end
