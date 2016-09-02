b5b19134902cf67d83da47c3f2faf8100ca10e9b
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-02 12:39:57 +0300

    Fix spec_halper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 
 #i added for test
 require 'shoulda/matchers'
-# require 'capybara/rspec'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
