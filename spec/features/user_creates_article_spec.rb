b2a2275623fd685aed75befc160045a6afb9a1ff
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-06 16:49:20 +0300

    Add test "allows user to visit to article page"

---
+++ spec/features/user_creates_article_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+# require 'database_cleaner'
+
+feature "Article Creation" do
+  before(:all) do
+    sign_up_1
+  end
+
+    scenario "allows user to visit to article page" do
+      visit new_article_path
+
+      expect(page).to have_content 'New article'
+    end
+end
