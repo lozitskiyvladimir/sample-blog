709d9af599278e3ad9c7e1dd26c2941ef6cf2884
Lozitskiy Vladimir <loza-w@mail.ru>
2016-09-08 14:23:46 +0300

    Fix before method

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
-# require 'database_cleaner'
+
 
 feature "Article Creation" do
-  before(:all) do
-    sign_up_1
+
+  before(:each) do
+    sign_up_helper
+
   end
 
     scenario "allows user to visit to article page" do
@@ -11,4 +13,17 @@ feature "Article Creation" do
 
       expect(page).to have_content I18n.t('articles.articles_new')
     end
+
+    scenario "allows user to visit to created article page" do
+      visit new_article_path
+
+
+      fill_in :article_title, :with => 'test_title'
+      fill_in :article_text, :with => 'example_text'
+      click_button 'Save Article'
+
+      expect(page).to have_content 'example_text'
+    end
+
+
 end
